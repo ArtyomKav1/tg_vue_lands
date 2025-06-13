5d9b3ba13ff87aa858c30523b7b204537c9bd4cd
Artyom K <artem.kavun90@gmail.com>
2025-06-13 16:10:06 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <div>tg</div>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
